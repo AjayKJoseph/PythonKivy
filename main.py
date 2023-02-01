2d8f48dd6d2379f3b73aadeef6b477ad7d793584
ajaykjoseph <ajaykjoseph@github.com>
2023-02-01 22:35:41 +0000

    First commit
    Button and Label example

---
+++ main.py
@@ -0,0 +1,12 @@
+from kivy.uix.widget import Widget
+from kivy.app import App
+
+class MainWidget(Widget):
+    pass
+
+class TheLabApp(App): # Add app at the end of the name
+    pass
+
+
+
+TheLabApp().run() # ensure to add parathensis () and run to instantiate class and create an object
