747932f77add7a69cd782034736e37203f35ef9e
ajaykjoseph <ajaykjoseph@github.com>
2023-02-05 10:31:28 +0000

    Test file for linux2

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from kivy.uix.widget import Widget
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
-git from kivy.uix.button import Button
+from kivy.uix.button import Button
 
 class BoxLayoutExample(BoxLayout):
     pass
