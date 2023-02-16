efbcdf1feb9e7e39594fbdd5f68de4c44d113999
ajaykjoseph <ajaykjoseph@github.com>
2023-02-16 07:18:04 +0000

    Python Kivy Tutorial Initial Commit - Version control from tutorial 6

---
+++ Tutoral4.py
@@ -0,0 +1,68 @@
+# https://www.youtube.com/watch?v=AxKksRhcmOA&list=PLCC34OHNcOtpz7PJQ7Tv7hqFBP_xDDjqg&index=4
+import kivy
+from kivy.app import App
+from kivy.uix.label import Label
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button
+
+class MyGridLayout(GridLayout):
+    def __init__(self, **kwargs):
+        super(MyGridLayout, self).__init__(**kwargs)
+        
+        # Create number of grids
+        self.cols = 1
+        
+        # Create a second GridLayout
+        self.top_grid = GridLayout()
+        self.top_grid.cols = 2
+
+        # Add a Label
+        self.top_grid.add_widget(Label(text = "Name: "))
+        # Create an Input box
+        self.name = TextInput(multiline=False)
+        self.top_grid.add_widget(self.name)
+        
+        # Add a Label
+        self.top_grid.add_widget(Label(text = "Test: "))
+        # Create an Input box
+        self.pizza = TextInput(multiline=False)
+        self.top_grid.add_widget(self.pizza)
+        
+        # Add a Label
+        self.top_grid.add_widget(Label(text = "Test2: "))
+        # Add an Input box
+        self.pizza1 = TextInput(multiline=False)
+        self.top_grid.add_widget(self.pizza1)
+        
+        # Add the new top_grid to our app.
+        self.add_widget(self.top_grid)
+        
+        # Create a Submit button
+        self.submit = Button(text="Submit", font_size=32, 
+                             size_hint_y = None, height = 100)
+        # Bind the Button
+        self.submit.bind(on_press=self.press) # Add a method
+        self.add_widget(self.submit)
+    
+    # Create an instance outside of __init__ to capture Input Texts
+    def press(self, instance):
+        name = self.name.text # Line 19
+        pizza = self.pizza.text
+        pizza2 = self.pizza1.text
+        
+        # Display name on the app
+        test =f'Hello {name}, you like {pizza} pizza, your favourite pizza2 is {pizza2}'
+        self.add_widget(Label(text=test))
+        
+        # Clear input boxes
+        self.name.text = ""
+        self.pizza.text = ""
+        self.pizza1.text = ""
+        
+class MyApp(App):
+    def build(self):
+        return MyGridLayout()
+    
+if __name__ == '__main__':
+    MyApp().run()
---
+++ Tutorial1.py
@@ -0,0 +1,11 @@
+# https://www.youtube.com/watch?v=dLgquj0c5_U&list=PLCC34OHNcOtpz7PJQ7Tv7hqFBP_xDDjqg
+import kivy
+from kivy.app import App
+from kivy.uix.label import Label
+
+class MyApp(App):
+    def build(self):
+        return Label(text = "Hello world")
+    
+if __name__ == '__main__':
+    MyApp().run()
